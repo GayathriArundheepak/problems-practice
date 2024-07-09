b2b7ea7bf7086bd72fe9d2d74c3eac6b4ae9a6bf
GayathriArundheepak <127670381+GayathriArundheepak@users.noreply.github.com>
2024-07-09 09:09:12 +0530

    array basic operations done

--- Array/operations.js
+++ Array/operations.js
@@ -71,17 +71,59 @@ let arr16 =[0, 1, 0, 1, 0];
 arr16 = arr16.map(num =>(num === 0 ? 1 : num))
 console.log(arr16)
 // 17.Given the array [5, 10, 15, 20, 25], insert 12 between 10 and 15.
-
-// Remove the second-to-last element from the array [2, 4, 6, 8, 10].
-// Replace the element at index 1 in the array [3, 6, 9] with 12.
-// Add the elements 100 and 200 at the beginning of the array [300, 400, 500].
-// Remove all occurrences of 42 from the array [42, 42, 42, 42, 42].
-// Insert the element 7 at the end of the array [1, 2, 3, 4, 5, 6].
+arr17=[5, 10, 15, 20, 25];
+index =arr17.indexOf(15)
+arr17.splice(index,0,12)
+console.log(arr17)
+// 18.Remove the second-to-last element from the array [2, 4, 6, 8, 10].
+arr18 =[2, 4, 6, 8, 10];
+arr18.splice(arr18.length -2,1)
+console.log(arr18)
+// 19.Replace the element at index 1 in the array [3, 6, 9] with 12.
+let arr19 =  [3, 6, 9]
+arr19[1]=12;
+console.log(arr19)
+//20. Add the elements 100 and 200 at the beginning of the array [300, 400, 500].
+arr20 =[300, 400, 500]
+arr20.unshift(100,200)
+console.log(arr20)
+//21. Remove all occurrences of 42 from the array [42, 42, 42, 42, 42].
+let arr21 = [42, 42, 42, 42, 42]
+arr21 =  arr21.filter(num => (num !== 42))
+console.log(arr21)
+// 22.Insert the element 7 at the end of the array [1, 2, 3, 4, 5, 6].
+let arr22 =[1, 2, 3, 4, 5, 6];
+arr22.push(7)
+console.log("22:",arr22)
 // Replace the last occurrence of 8 with 80 in the array [8, 16, 24, 8, 32].
+let arr23 =[8, 16, 24, 8, 32];
+let index23=arr23.lastIndexOf(8)
+console.log("23:",index23)
 // Given the array [2, 4, 6, 8, 10], insert 5 between 4 and 6.
-// Remove all elements from the array [11, 22, 33, 44, 55].
-// Replace the first element in the array [7, 14, 21] with 10.
-// Add the elements 1 and 2 at the end of the array [3, 4, 5].
-// Remove the element at index 0 from the array [50, 40, 30, 20, 10].
-// Replace all occurrences of 6 with 60 in the array [6, 12, 18, 6, 30].
-// Given the array [3, 6, 9, 12, 15], insert 7 at index 3.
+let arr24 = [2, 4, 6, 8, 10];
+arr24.splice(2,0,5)
+console.log("24:",arr24)
+//25. Remove all elements from the array [11, 22, 33, 44, 55].
+let arr25=[11, 22, 33, 44, 55];
+arr25.length =0;
+console.log("25:",arr25)
+// 26.Replace the first element in the array [7, 14, 21] with 10.
+let arr26 =[7, 14, 21];
+arr26[0]=10
+console.log("26:",arr26)
+// 27.Add the elements 1 and 2 at the end of the array [3, 4, 5].
+let arr27 =[3,4,5]
+arr27.push(1,2)
+console.log("27:",arr27)
+// 28.Remove the element at index 0 from the array [50, 40, 30, 20, 10].
+let arr28 =[50, 40, 30, 20, 10];
+arr28.shift()
+console.log("28:",arr28)
+// 29.Replace all occurrences of 6 with 60 in the array [6, 12, 18, 6, 30].
+let arr29 =[6,12,18,6,30];
+arr29 =arr29.map(num=>(num ===6 ?60 :num))
+console.log("29:",arr29)
+// 30.Given the array [3, 6, 9, 12, 15], insert 7 at index 3.
+let arr30 =[3, 6, 9, 12, 15];
+arr30[3]=7
+console.log("30:",arr30)
